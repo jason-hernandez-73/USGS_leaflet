ccb7032452f0e45d9dff66efe33d56aa6ec72fc4
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-05-03 11:14:27 -0700

    Skeleton code

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,22 @@
 // Get data
+const url='https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson';
 
 // Create base map
+var mymap = L.map('map').setView([x, -x], x);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
 
 // Add layers
+var circle = L.circle([x, x], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: x
+}).addTo(mymap);
