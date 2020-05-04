5fd7c056a922e186c846a2f13b99afd37c400842
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-05-04 14:25:30 -0700

    Format code

--- static/js/logic.js
+++ static/js/logic.js
@@ -15,41 +15,41 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(mymap);
 
 // Add layers
-d3.json(url, function(data){
+d3.json(url, function (data) {
     console.log(data.features);
 
     function markerLocation(coord) {
-        var latLng=[];
+        var latLng = [];
         latLng.push(coord[1]);
         latLng.push(coord[0]);
         return latLng;
     };
 
     function markerSize(magnitude) {
-        var magn=[];
+        var magn = [];
         magn.push(magnitude);
-        if(magn>6.5){
-            return magn*50000;
+        if (magn > 6.5) {
+            return magn * 50000;
         }
-        else if(magn>6){
-            return magn*25000;
+        else if (magn > 6) {
+            return magn * 25000;
         }
-        else if(magn>5){
-            return magn*10000;
+        else if (magn > 5) {
+            return magn * 10000;
         }
         else {
-            return magn*5000;
+            return magn * 5000;
         }
-        
+
     };
 
-    features=data.features
+    features = data.features
     for (var i = 0; i < features.length; i++) {
         L.circle(markerLocation(features[i].geometry.coordinates), {
             color: 'red',
             fillColor: '#f03',
             fillOpacity: 0.5,
             radius: markerSize(features[i].properties.mag)
-        }).bindPopup('<h1>'+features[i].properties.place+'</h1><hr><h3> Magnitude: '+features[i].properties.mag+'</h3><br></h3>Time:'+features[i].properties.time).addTo(mymap);
+        }).bindPopup('<h1>' + features[i].properties.place + '</h1><hr><h3> Magnitude: ' + features[i].properties.mag + '</h3><br></h3>Time:' + features[i].properties.time).addTo(mymap);
     };
 });
