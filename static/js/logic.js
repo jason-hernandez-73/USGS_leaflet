0474c724cf1c29430d6fb1f3f98d90ace45d00d4
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-05-03 21:33:08 -0700

    Switched tilset, added markers

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,33 +1,43 @@
 // Get data
 const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson';
 
+
 // Create base map
-var mymap = L.map('map').setView([0.00, 0.00], 1);
+var mymap = L.map('map').setView([0.00, 0.00], 3);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/outdoors-v11',
+    id: 'mapbox/emerald-v8',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'API_KEY'
+    accessToken: API_KEY
 }).addTo(mymap);
 
 // Add layers
-function markerLocation(coord) {
-    return coordinates;
-}
-function markerSize(magnitude) {
-    return mag * 10;
-};
+d3.json(url, function(data){
+    console.log(data.features);
 
+    function markerLocation(coord) {
+        var latLng=[];
+        latLng.push(coord[1]);
+        latLng.push(coord[0]);
+        return latLng;
+    };
 
-for (var i = 0; i < features.length; i++) {
-    L.circle(features[i].coord, {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: markerSize(features[i].mag)
-    }).bindPopup('<h1>'+features[i].place+'</h1><hr><h3> Magnitude: '+features[i].mag+'</h3><br></h3>Time:'+features[i].time).addTo(mymap);
+    function markerSize(magnitude) {
+        var magn=[];
+        magn.push(magnitude[2]);
+        return magn * 10;
+    };
 
-}
+    features=data.features
+    for (var i = 0; i < features.length; i++) {
+        L.circle(markerLocation(features[i].geometry.coordinates), {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: markerSize(features[i].geometry.coordinates)
+        }).bindPopup('<h1>'+features[i].properties.place+'</h1><hr><h3> Magnitude: '+features[i].properties.mag+'</h3><br></h3>Time:'+features[i].properties.time).addTo(mymap);
+    };
+});
